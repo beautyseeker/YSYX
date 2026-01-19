6b96f060764c3d7500a50b25b727230691e051b3
Jefree <983466882@qq.com>
2026-01-19 22:11:14 +0800

    实现nvboard上板验证和gtk记录波形

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -8,57 +8,70 @@
 #include <verilated_vcd_c.h>
 #include <string>
 #include <filesystem>
+#include <nvboard.h>
+
+
+static TOP_NAME dut;
+void nvboard_bind_all_pins(TOP_NAME* top);
+void vcd_init(VerilatedVcdC* tfp, VerilatedContext* contextp, Vtop* topp, const char* exe_path);
 
 //======================
 
 int main(int argc, char** argv, char**) {
-    // Setup context, defaults, and parse command line
     Verilated::debug(0);
     const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
     contextp->commandArgs(argc, argv);
-
-    // Construct the Verilated model, from Vtop.h generated from Verilating
-    const std::unique_ptr<Vtop> topp{new Vtop{contextp.get()}};
     VerilatedVcdC* tfp = new VerilatedVcdC;
-    contextp->traceEverOn(true);
-    topp->trace(tfp, 99);
-    
-    std::string exe_name = std::filesystem::path(argv[0]).filename().string();
-    std::string vcd_name = "build/waveforms/" + exe_name + ".vcd";
-    tfp->open(vcd_name.c_str());
 
-    uint8_t a = 0;
-    uint8_t b = 0;
-    vluint64_t max_time = 5000;
+    vluint64_t max_time = 0;
     if(argc > 1) {
         max_time = atoi(argv[1]);
     }
 
-    // Simulate until $finish or max_time reached
-    while (!contextp->gotFinish() && contextp->time() < max_time) {
-        // Evaluate model
+    const std::unique_ptr<Vtop> topp{new Vtop{contextp.get()}};
+    if(max_time > 0) {
+      vcd_init(tfp, contextp.get(), topp.get(), argv[0]);
+    }
+
+
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+
+    uint8_t a = 0;
+    uint8_t b = 0;
+
+    while (!contextp->gotFinish()) {
         a = rand() & 1;
         b = rand() & 1;
+        // a = nvboard_get_pin_value("TOP.a");
+        // b = nvboard_get_pin_value("TOP.b");
         topp->a = a;
         topp->b = b;
         topp->eval();
-        tfp->dump(contextp->time());
+        
+        if(contextp->time() <= max_time)
+          tfp->dump(contextp->time());
         VL_DEBUG_IF(VL_PRINTF("+ Time: %llu ns | a=%d b=%d f=%d\n",
                                 contextp->time(), topp->a, topp->b, topp->f););
-        
-        
         assert(topp->f == (a ^ b));
-        // Advance time
+
+        dut.eval();
+        nvboard_update();
         contextp->timeInc(1);
-
     }
 
-    if (!contextp->gotFinish()) {
-        VL_DEBUG_IF(VL_PRINTF("+ Exiting without $finish; no events left\n"););
-    }
-
-    // Final model cleanup
     topp->final();
-    tfp->close();
+    if(max_time > 0 && tfp != nullptr) {
+      tfp->close();
+      delete tfp;
+    }
     return 0;
+}
+
+void vcd_init(VerilatedVcdC* tfp, VerilatedContext* contextp, Vtop* topp, const char* exe_path) {
+    contextp->traceEverOn(true);
+    topp->trace(tfp, 99);
+    std::string exe_name = std::filesystem::path(exe_path).filename().string();
+    std::string vcd_name = "build/waveforms/" + exe_name + ".vcd";
+    tfp->open(vcd_name.c_str());
 }
