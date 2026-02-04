25dd07e5fb0008fa1240c63537e4cbb11b85a7c0
Jefree <983466882@qq.com>
2026-02-04 11:15:31 +0800

    完成sCPU上板和功耗评估,E5完结

---
+++ npc/report/sCPU-100MHz/sCPU.v
@@ -0,0 +1,79 @@
+//Generate the verilog at 2026-02-04T10:56:48 by iSTA.
+module sCPU (
+clk,
+rst,
+out_seg0_0_,
+out_seg0_1_,
+out_seg0_2_,
+out_seg0_3_,
+out_seg0_4_,
+out_seg0_5_,
+out_seg0_6_,
+out_seg0_7_,
+out_seg1_0_,
+out_seg1_1_,
+out_seg1_2_,
+out_seg1_3_,
+out_seg1_4_,
+out_seg1_5_,
+out_seg1_6_,
+out_seg1_7_
+);
+
+input clk ;
+input rst ;
+output out_seg0_0_ ;
+output out_seg0_1_ ;
+output out_seg0_2_ ;
+output out_seg0_3_ ;
+output out_seg0_4_ ;
+output out_seg0_5_ ;
+output out_seg0_6_ ;
+output out_seg0_7_ ;
+output out_seg1_0_ ;
+output out_seg1_1_ ;
+output out_seg1_2_ ;
+output out_seg1_3_ ;
+output out_seg1_4_ ;
+output out_seg1_5_ ;
+output out_seg1_6_ ;
+output out_seg1_7_ ;
+
+wire clk ;
+wire rst ;
+wire out_seg0_0_ ;
+wire out_seg0_1_ ;
+wire out_seg0_2_ ;
+wire out_seg0_3_ ;
+wire out_seg0_4_ ;
+wire out_seg0_5_ ;
+wire out_seg0_6_ ;
+wire out_seg0_7_ ;
+wire out_seg1_0_ ;
+wire out_seg1_1_ ;
+wire out_seg1_2_ ;
+wire out_seg1_3_ ;
+wire out_seg1_4_ ;
+wire out_seg1_5_ ;
+wire out_seg1_6_ ;
+wire out_seg1_7_ ;
+
+assign out_seg0_0_ = out_seg0_2_ ;
+assign out_seg0_0_ = out_seg0_3_ ;
+assign out_seg0_0_ = out_seg0_4_ ;
+assign out_seg0_0_ = out_seg0_5_ ;
+assign out_seg0_0_ = out_seg0_6_ ;
+assign out_seg0_0_ = out_seg0_7_ ;
+assign out_seg0_0_ = out_seg1_0_ ;
+assign out_seg0_1_ = out_seg1_1_ ;
+assign out_seg0_0_ = out_seg1_2_ ;
+assign out_seg0_0_ = out_seg1_3_ ;
+assign out_seg0_0_ = out_seg1_4_ ;
+assign out_seg0_0_ = out_seg1_5_ ;
+assign out_seg0_0_ = out_seg1_6_ ;
+assign out_seg0_0_ = out_seg1_7_ ;
+
+TIEHIH7L out_seg0_TIEHIH7L_Z ( .Z(out_seg0_1_ ) );
+TIELOH7L out_seg0_TIELOH7L_Z ( .Z(out_seg0_0_ ) );
+
+endmodule
